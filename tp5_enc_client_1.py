c9a865ec1baf4b0e8023a8d27ba91e80837d6502
lingrid <ingrid.lare@ynov.com>
2024-10-30 16:54:46 +0100

    nouveau fichier

--- tp5_enc_client_1.py
+++ tp5_enc_client_1.py
@@ -1,3 +1,19 @@
-h = 45
-print(h)
+import socket
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9999))
+s.send('Hello'.encode())
+
+# On reçoit la string Hello
+data = s.recv(1024)
+
+# Récupération d'une string utilisateur
+msg = input("Calcul à envoyer: ")
+
+# On envoie
+s.send(msg.encode())
+
+# Réception et affichage du résultat
+s_data = s.recv(1024)
+print(s_data.decode())
+s.close()
