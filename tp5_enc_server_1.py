dd4b6f26acf87942c7df3306fd18839df4b2e342
lingrid <ingrid.lare@ynov.com>
2024-10-30 17:29:51 +0100

    nouveau fichier

--- tp5_enc_server_1.py
+++ tp5_enc_server_1.py
@@ -1,10 +1,10 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('127.0.0.1', 8888))
+sock.bind(('127.0.0.1', 9999))
 sock.listen()
 
-print("le serveur est en attente de connexion sur le port 8888")
+print("le serveur est en attente de connexion sur le port 9999")
 
 client, client_addr = sock.accept()
 
