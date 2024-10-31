302166ccac7437990102fbf83b1927791e51a85d
lingrid <ingrid.lare@ynov.com>
2024-10-31 15:08:39 +0100

    nouveau fichier

--- tp5_enc_server_1.py
+++ tp5_enc_server_1.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('127.0.0.1', 9999))
+sock.bind(('10.1.1.2', 8888))
 sock.listen()
 client, client_addr = sock.accept()
 
