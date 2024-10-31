0d0f5afacef25da281abda574629725aa21d4585
lingrid <ingrid.lare@ynov.com>
2024-10-31 15:20:45 +0100

    nouveau fichier

--- tp5_enc_client_1.py
+++ tp5_enc_client_1.py
@@ -1,19 +1,23 @@
-import socket
+import socket 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('10.1.1.2', 8888))
-s.send('Hello'.encode())
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('127.0.0.1', 9999))
 
-# On reçoit la string Hello
-data = s.recv(1024)
+# on récup une string saisie par l'utilisateur
+msg = input('Enter a message: ')
 
-# Récupération d'une string utilisateur
-msg = input("Calcul à envoyer: ")
+# on encode le message explicitement en UTF-8 pour récup un tableau de bytes
+encoded_msg = msg.encode('utf-8')
 
-# On envoie
-s.send(msg.encode())
+# on calcule sa taille, en nombre d'octets
+msg_len = len(encoded_msg)
 
-# Réception et affichage du résultat
-s_data = s.recv(1024)
-print(s_data.decode())
-s.close()
+# on encode ce nombre d'octets sur une taille fixe de 4 octets
+header = msg_len.to_bytes(4, byteorder='big')
+
+# on peut concaténer ce header avec le message, avant d'envoyer sur le réseau
+payload = header + encoded_msg
+
+# on peut envoyer ça sur le réseau
+sock.send(payload)
+sock.close()
