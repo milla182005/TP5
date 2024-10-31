070f6374c8c5fd1c58731e0584a5f471ec428635
lingrid <ingrid.lare@ynov.com>
2024-10-31 15:14:17 +0100

    nouveau fichier

--- tp5_enc_client_1.py
+++ tp5_enc_client_1.py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 9999))
+s.connect(('127.0.0.1', 8888))
 s.send('Hello'.encode())
 
 # On reçoit la string Hello
