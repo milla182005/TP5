c1ac7c9aed5c1d6a8011490b8d5a81bd932dbe21
lingrid <ingrid.lare@ynov.com>
2024-10-31 14:56:28 +0100

    nouveau fichier

--- tp5_enc_client_1.py
+++ tp5_enc_client_1.py
@@ -2,24 +2,22 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('127.0.0.1', 9999))
-sock.send("Hello" .encode())
 
-data = sock.recv(1024)
+# on récup une string saisie par l'utilisateur
+msg = input('Enter a message: ')
 
-msg = input('Calcul a envoyé: ')
-
-sock.send(msg.encode())
-
+# on encode le message explicitement en UTF-8 pour récup un tableau de bytes
 encoded_msg = msg.encode('utf-8')
 
+# on calcule sa taille, en nombre d'octets
 msg_len = len(encoded_msg)
 
+# on encode ce nombre d'octets sur une taille fixe de 4 octets
 header = msg_len.to_bytes(4, byteorder='big')
 
+# on peut concaténer ce header avec le message, avant d'envoyer sur le réseau
 payload = header + encoded_msg
 
-sock_data = sock.recv(1024)
-print(sock_data.decode())
-
+# on peut envoyer ça sur le réseau
 sock.send(payload)
 sock.close()
--- tp5_enc_server_1.py
+++ tp5_enc_server_1.py
@@ -1,12 +1,8 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('127.0.0.1', 9999))
-
-sock.listen(1)
-conn, addr = sock.accept()
-
+sock.listen()
 client, client_addr = sock.accept()
 
 while True:
